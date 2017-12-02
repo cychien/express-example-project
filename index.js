b4089c80556fd76252456d45bdaa95ba782fbba7
cychien <xyz030206@gmail.com>
2017-12-03 01:32:12 +0800

    initial steps to create express project

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
